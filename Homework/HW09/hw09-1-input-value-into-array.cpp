919f325672dc4ae192831e431eef6ea346fab0fe
wachilanee <wachilanee47@gmail.com>
2023-09-14 11:41:48 +0700

    hw9-1

--- Homework/HW09/hw09-1-input-value-into-array.cpp
+++ Homework/HW09/hw09-1-input-value-into-array.cpp
@@ -23,5 +23,29 @@
     Output:
         Index = 0 1 2 3 4 5 6 7
         Array = 1 1 3 9 8 2 7 8
-
 */
+
+#include <stdio.h>
+int main() {
+    
+    int num[ 100 ], N = 0, i;
+
+    for ( i = 0 ; i <= N ; i++ ){
+        printf( "Input [%d] :\n",i ) ;
+        scanf( "%d", &num[ i ] ) ;
+        N++ ;
+        if ( num[ i ] == -1 ) break;
+    }
+
+    printf( "Index = " ) ;
+    for ( i = 0 ; i < N - 1 ; i++ ) {
+        printf( " %d",i );
+    }
+
+    printf( "\nArray = " ) ;
+    for ( i = 0; i < N - 1 ; i++){
+        printf( " %d", num[ i ] ) ;
+    }
+    
+    return 0 ;
+}
